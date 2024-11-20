c99b7e6354c3268d1e8dc9ba6571f5245b64b0f4
IAMHUDA <hudamiftahul362@gmail.com>
2024-11-20 23:28:12 +0700

    menambahkan Scaffold pada navigasi

--- app/src/main/java/com/example/pertemuan7/ui/viewmodel/NavigationControl.kt
+++ app/src/main/java/com/example/pertemuan7/ui/viewmodel/NavigationControl.kt
@@ -1,8 +1,18 @@
 package com.example.pertemuan7.ui.viewmodel
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.pertemuan7.model.ListGender
+import com.example.pertemuan7.ui.view.DetailMahasiswaView
+import com.example.pertemuan7.ui.view.FormMahasiswaView
 
 enum class Halaman{
     Form,
@@ -13,7 +23,15 @@ enum class Halaman{
 fun Navigasi(
     modifier: Modifier = Modifier,
     viewModel: MahasiswaViewModel = viewModel(),
-    navHost: navHostController = rememberNavController()
-) {
+    navHost: NavHostController = rememberNavController()
+){
+    Scaffold { isipadding ->
+        val _dataModel by viewModel.dataModel.collectAsState()
+        NavHost(
+            modifier = modifier.padding(isipadding),
+            navController = navHost, startDestination = Halaman.Form.name
+        ){
 
+        }
+    }
 }
