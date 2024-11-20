956a3ca25c63c77cf97254dc7d0e121b0e2b7e68
IAMHUDA <hudamiftahul362@gmail.com>
2024-11-20 22:04:55 +0700

    menambahkan fungsi navigasi

--- app/src/main/java/com/example/pertemuan7/ui/viewmodel/NavigationControl.kt
+++ app/src/main/java/com/example/pertemuan7/ui/viewmodel/NavigationControl.kt
@@ -1,6 +1,19 @@
 package com.example.pertemuan7.ui.viewmodel
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.rememberNavController
+
 enum class Halaman{
     Form,
     Data
+}
+
+@Composable
+fun Navigasi(
+    modifier: Modifier = Modifier,
+    viewModel: MahasiswaViewModel = viewModel(),
+    navHost: navHostController = rememberNavController()
+) {
+
 }
